1adee1d4125b7eb4552a3196fa962b83e15f0cea
KarbitsCode <107671693+KarbitsCode@users.noreply.github.com>
2026-05-12 20:54:02 +0800

    Add make.bat

---
+++ ThirdVendor/protobuf-2.5.0/vsprojects/make.bat
@@ -0,0 +1,4 @@
+msbuild protobuf-lite.slnf /t:Build /p:Configuration=Debug /p:Platform=Win32
+msbuild protobuf-lite.slnf /t:Build /p:Configuration=Debug /p:Platform=x64
+msbuild protobuf-lite.slnf /t:Build /p:Configuration=Release /p:Platform=Win32
+msbuild protobuf-lite.slnf /t:Build /p:Configuration=Release /p:Platform=x64
---
+++ make.bat
@@ -0,0 +1,6 @@
+pushd ThirdVendor\protobuf-2.5.0\vsprojects
+cmd /c make.bat
+popd
+msbuild PianoFromAbove.sln /t:Build /p:Configuration=Release /p:Platform=Win32
+msbuild PianoFromAbove.sln /t:Build /p:Configuration=Release /p:Platform=x64
+makensis /V4 PianoFromAbove.nsi
