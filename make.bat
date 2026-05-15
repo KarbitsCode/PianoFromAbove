1f98b7c33c04286dacb9f390d356a64f9b7596ee
KarbitsCode <107671693+KarbitsCode@users.noreply.github.com>
2026-05-15 13:48:06 +0800

    Update make.bat

--- make.bat
+++ make.bat
@@ -1,6 +1,8 @@
-pushd ThirdVendor\protobuf-2.5.0\vsprojects
-cmd /c make.bat
-popd
+if not exist ThirdVendor\protobuf-2.5.0\vsprojects\Release (
+  pushd ThirdVendor\protobuf-2.5.0\vsprojects
+  cmd /c make.bat
+  popd
+)
 msbuild PianoFromAbove.sln /t:Build /p:Configuration=Release /p:Platform=Win32
 msbuild PianoFromAbove.sln /t:Build /p:Configuration=Release /p:Platform=x64
 makensis /V4 PianoFromAbove.nsi
